8d2adfa33c04ab61e6b240ab243d1a33a19d5956
HTMLhead <mrdoo309@gmail.com>
2019-09-29 18:11:02 +0900

    feat(env.template): 환경변수 설정

--- src/Scripts/PageRouter/index.tsx
+++ src/Scripts/PageRouter/index.tsx
@@ -7,9 +7,7 @@ const PageRouter: React.FC<any> = () => {
   return (
     <Router>
       <>
-        <Switch>
-          <Route exact path="" component={} />
-        </Switch>
+        <Switch></Switch>
       </>
     </Router>
   );
